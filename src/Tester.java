58ce86d24959bf75fc5ae2eb0fb7bf973d638427
gdamelin <66569945+gdamelin@users.noreply.github.com>
2021-09-05 18:35:55 -0700

    Attempted to make Tester class

--- src/Tester.java
+++ src/Tester.java
@@ -1,4 +1,16 @@
+import java.io.IOException;
+import java.util.ArrayList;
 
-public class Tester {
-
+public class Tester
+{
+	public static void main(String[] args) throws IOException
+	{
+		LZW compressor = new LZW ();
+		
+		ArrayList<Character> charList = compressor.makeArrayList("lzw-file3.txt");
+		ArrayList<Integer> intList = compressor.compress(charList);
+		System.out.println (intList);
+		String word = compressor.decompress(intList);
+		System.out.println (word);
+	}
 }
