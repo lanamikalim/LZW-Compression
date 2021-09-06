6341150aa786ec16aa68636c61d3259c2afafeff
gdamelin <66569945+gdamelin@users.noreply.github.com>
2021-09-05 17:59:45 -0700

    Completed Decompress method

--- src/LZW.java
+++ src/LZW.java
@@ -43,15 +43,37 @@ public class LZW
 	
 	public String decompress (ArrayList<Integer> compressed)
 	{
-		Map<String, Integer> dictionary = new HashMap<String, Integer>();
+		Map<Integer, String> dictionary = new HashMap<Integer, String>();
 		
 		int dictionarySize = 256;
 		
 		for (int i = 0; i < 256; i++)
 		{
-			dictionary.put("" + (char)i, i);
+			dictionary.put(i, "" + (char)i);
 		}
 		
+		String currentLetters = "" + (char)(int)compressed.remove(0);
 		
+		for (int letters : compressed)
+		{
+			String dictionaryEntry;
+			
+			if (dictionary.containsKey(letters))
+			{
+				dictionaryEntry = dictionary.get(letters);
+			}
+			else
+			{
+				dictionaryEntry = currentLetters + currentLetters.charAt(0);
+			}
+			
+			currentLetters = currentLetters + dictionaryEntry;
+			
+			dictionary.put(dictionarySize++, currentLetters + dictionaryEntry.charAt(0));
+			
+			currentLetters =  dictionaryEntry;
+		}
+		
+		return (currentLetters);
 	}
 }
